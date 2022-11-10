3c6f08d817c0dffd74dab49990809e62e809b4db
Lilium-Github <66401081+Lilium-Github@users.noreply.github.com>
2022-11-10 14:49:34 -0700

    Update linkedLists.py

--- linkedLists.py
+++ linkedLists.py
@@ -1,50 +1,41 @@
-class Student:
-    def __init__(self, name, ID, cool, next=None, last=None):
-        self.name = name
-        self.ID = ID
+class Node:
+    def __init__(self, data, next=None, last=None):
+        self.data = data
         self.last = last
         self.next = next
-        self.cool = cool
 
-class LinkedList:
+class L_List:
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
 
-    def insert(self, name, ID, cool):
+    def insert(self, data):
         self.length += 1
-        newStudent = Student(name, ID, cool)
+        newNode = Node(data)
         if self.head: 
             current = self.head
             while current.next:
                 current = current.next
-            current.next = newStudent
-            newStudent.last = current
+            current.next = newNode
+            newNode.last = current
         else:
-            self.head = newStudent
-        self.tail = newStudent
+            self.head = newNode
+        self.tail = newNode
 
-    def printList(self, order='f'):
+    def print(self, order='f'):
         if order == 'f': #f for forwards
             current = self.head
             while current:
-                print("Student name:", current.name + ", ID:", current.ID, end=". ")
-                if current.cool:
-                    print("They're very cool.")
-                else:
-                    print("They're kinda lame.")
+                print(current.data, end = " ")
                 current = current.next
         elif order == 'b': #b for backwards
             current = self.tail
             while current:
-                print("Student name:", current.name + ", ID:", current.ID, end=". ")
-                if current.cool:
-                    print("They're very cool.")
-                else:
-                    print("They're kinda lame.")
+                print(current.data, end = " ")
                 current = current.last
 
+    
     def search(self, name):
         current = self.head
         while current:
@@ -54,7 +45,7 @@ class LinkedList:
         return False
 
     def index(self, index_num):
-        if index_num >= self.length:
+        if index_num > self.length:
             print("error: index out of range")
             return False
         else:
@@ -68,6 +59,7 @@ class LinkedList:
     def pop(self):
         self.length -= 1
 
+        self.head.next.last = None
         self.head = self.head.next
 
     def snip(self):
@@ -80,23 +72,16 @@ class LinkedList:
         self.tail = current
 
     def remove(self, index_num):
-        if index_num >= self.length:
+        if index_num > self.length:
             print("error: index out of range")
             return False
         else:
             current = self.head
             while current:
-                if index_num == 1:
+                if index_num == 0:
                     current.next = current.next.next
                     break
                 index_num -= 1
                 current = current.next
 
-sLine = LinkedList()
-sLine.insert("Lily", 889681, False)
-sLine.insert("Kyle", 666666, True)
-sLine.insert("Mossy", 123456, True)
-sLine.insert("Cory", 4, True)
-print(sLine.printList('b'), "\n\n")
-sLine.snip()
-print(sLine.printList('b'))
+def swap_adj(link):
